dfc589beef915a36da472290d3de1538e4e56223
Rob Kano <deathbydissection@gmail.com>
2019-11-05 23:30:20 +0400

    login view commit

--- app/models.py
+++ app/models.py
@@ -3,9 +3,10 @@ from werkzeug.security import check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
 from app import db
+from app import login
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -30,3 +31,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return f"<Post {self.body}>"
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
